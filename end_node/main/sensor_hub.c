cdfbc80252d73a91d0b962a2125d821c3008deb8
vxnwt846 <benhassenmahdi@gmail.com>
2026-05-25 15:13:31 +0100

    fix(end_node): use curve fitting calibration and default clock source for ESP32-H2 ADC

--- end_node/main/sensor_hub.c
+++ end_node/main/sensor_hub.c
@@ -711,7 +711,7 @@ esp_err_t sensor_hub_init(void)
 #if CONFIG_ENABLE_SOIL_MOISTURE
     adc_oneshot_unit_init_cfg_t init_config1 = {
         .unit_id = ADC_UNIT_1,
-        .clk_src = ADC_RTC_CLK_SRC_DEFAULT,
+        .clk_src = 0,
     };
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &s_adc1_handle));
 
@@ -722,12 +722,13 @@ esp_err_t sensor_hub_init(void)
     ESP_ERROR_CHECK(adc_oneshot_config_channel(s_adc1_handle, (adc_channel_t)SOIL_MOISTURE_ADC_CHANNEL, &config));
 
     /* Calibration scheme setup */
-    adc_cali_line_fitting_config_t cali_config = {
+    adc_cali_curve_fitting_config_t cali_config = {
         .unit_id = ADC_UNIT_1,
+        .chan = (adc_channel_t)SOIL_MOISTURE_ADC_CHANNEL,
         .atten = ADC_ATTEN_DB_12,
         .bitwidth = ADC_BITWIDTH_DEFAULT,
     };
-    if (adc_cali_create_scheme_line_fitting(&cali_config, &s_adc1_cali_handle) == ESP_OK) {
+    if (adc_cali_create_scheme_curve_fitting(&cali_config, &s_adc1_cali_handle) == ESP_OK) {
         s_adc_cali_enabled = true;
     }
 #endif
